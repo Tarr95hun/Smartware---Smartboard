eab8ca0b079343ea12ad0718d8e11beea1b48e90
X-D4RK\Tarr <tarr_zsombi@gmail.com>
2018-04-25 13:05:22 +0300

    popin php init

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,4 @@
-// Load google charts
+// Load google charts - start
 google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(drawChart);
 
@@ -19,4 +19,17 @@ function drawChart() {
   // Display the chart inside the <div> element with id="piechart"
   var chart = new google.visualization.PieChart(document.getElementById('chart'));
   chart.draw(data, options);
+}
+// Load google charts - end
+
+var getParagraphEditableTitle = function() {
+    var paragraph = document.getElementsByClassName("popin-txt")[0].children;
+    var txt = "";
+    for(var i=0;i<paragraph.length;i++) {
+      txt+=paragraph[i].textContent;
+      txt+="\n \n";
+    }
+    //document.getElementsByName('popin-txt-textarea')[0].appendChild(txt);
+    var x = document.getElementsByClassName("popin-title-textarea");
+    console.log(x);
 }
